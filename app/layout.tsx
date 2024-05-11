fbe5084178dff1671dfe9bfebe8109422e73a17b
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-11 19:02:16 +0200

    Fix: Enhance through fixing styles/fonts and adding `Hero` component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
-// DONE REVIEWING: GITHUB COMMIT 🥊
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+import {Poppins} from "next/font/google"
 import {ReactNode} from "react"
+import {Hero} from "../components"
+import "../styles/globals.css"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"]
+})
 
 type LayoutProps = {
   children: ReactNode
@@ -8,8 +16,8 @@ type LayoutProps = {
 const Layout = function Layout({children}: LayoutProps) {
   return (
     <html lang="en">
-      <body>
-        <nav>Navigation</nav>
+      <body className={poppins.className}>
+        <Hero />
         {children}
         <footer>Footer</footer>
       </body>
---
+++ components/index.ts
@@ -0,0 +1,2 @@
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+export {default as Hero} from "./hero"
