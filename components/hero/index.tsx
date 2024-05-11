a42dbbc8a4a4d95cff9d9620040b132b6385a5b4
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-11 19:12:45 +0200

    New: Enhance through adding navigation's links/state/logo

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,6 +1,36 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
+
+"use client"
+
+import Link from "next/link"
+import {useState} from "react"
+
+const navigation = [
+  {name: "Sauces", href: "/sauces"},
+  {name: "Menu", href: "/menu"},
+  {name: "Franchise", href: "/franchise"},
+  {name: "Contact", href: "/contact"}
+]
+
 const Hero = function Hero() {
-  return <div>Hero</div>
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  return (
+    <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          aria-label="Global"
+          className="mx-auto flex max-w-xl-7 items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <Link
+              href="/"
+              className="text-neutral-950 -m-1.5 p-1.5 text-lg font-semi-bold uppercase leading-none tracking-widest">
+              <span className="text-red-600">Krispy</span> King.
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
 }
 
 export default Hero
